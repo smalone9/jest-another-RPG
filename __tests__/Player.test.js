82e04f7cf7457135a0bbf6bc2fb986ef03841063
Samantha Malone <sammac99@gmail.com>
2021-06-30 12:23:58 -0700

    added to player health methods

--- __tests__/Player.test.js
+++ __tests__/Player.test.js
@@ -50,3 +50,16 @@ test('creates a player object', () => {
   
     expect(player.isAlive()).toBeFalsy();
   });
+
+  test("subtracts from player's health", () => {
+    const player = new Player('Dave');
+    const oldHealth = player.health;
+  
+    player.reduceHealth(5);
+  
+    expect(player.health).toBe(oldHealth - 5);
+  
+    player.reduceHealth(99999);
+  
+    expect(player.health).toBe(0);
+  });
