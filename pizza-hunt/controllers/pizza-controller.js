2c9f522fba4a8f1cdc2ac45543b8eaccf4013f14
Samantha Malone <sammac99@gmail.com>
2021-08-25 12:35:01 -0700

    finished user model

--- pizza-hunt/controllers/pizza-controller.js
+++ pizza-hunt/controllers/pizza-controller.js
@@ -8,31 +8,24 @@ const pizzaController = {
       .then((dbPizzaData) => res.json(dbPizzaData))
       .catch((err) => {
         console.log(err);
-        res.status(400).json(err);
+        res.sendStatus(400);
       });
   },
 
   // get one pizza by id
   getPizzaById({ params }, res) {
     Pizza.findOne({ _id: params.id })
-      .then((dbPizzaData) => {
-        // If no pizza is found, send 404
-        if (!dbPizzaData) {
-          res.status(404).json({ message: "No pizza found with this id!" });
-          return;
-        }
-        res.json(dbPizzaData);
-      })
+      .then((dbPizzaData) => res.json(dbPizzaData))
       .catch((err) => {
         console.log(err);
-        res.status(400).json(err);
+        res.sendStatus(400);
       });
   },
   // createPizza
   createPizza({ body }, res) {
     Pizza.create(body)
       .then((dbPizzaData) => res.json(dbPizzaData))
-      .catch((err) => res.status(400).json(err));
+      .catch((err) => res.json(err));
   },
 
   // update pizza by id
